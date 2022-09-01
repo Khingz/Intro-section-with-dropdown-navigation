1ee0d92543419b601c61e6d6346fc2a0617d8d15
Khingz <kingsley995mr@gmail.com>
2022-09-01 21:58:00 +0100

    body opacity fixed, mobile nav width adjusted

--- js/app.js
+++ js/app.js
@@ -2,13 +2,16 @@ const openBtn = document.querySelector('.toggle-icon')
 const closeBtn = document.querySelector('.close-menu')
 const mobileNav = document.querySelector('.mobile-nav')
 const navItemBtn = document.querySelectorAll('.mobile-nav-content')
+const body = document.querySelector('.body')
 
 const openSideNav = () => {
     mobileNav.style.right = '0%';
+    body.style.opacity ='0.2'
 }
 
 const closeSideNav = () => {
     mobileNav.style.right = '-100%';
+    body.style.opacity ='9'
 }
 openBtn.addEventListener('click', openSideNav)
 closeBtn.addEventListener('click', closeSideNav)
